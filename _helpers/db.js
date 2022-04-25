bc011d719144d2107c53482ff75e14f85a7dfa90
Janvi Dankhara <janvidankhara@Janvis-MacBook-Pro.local>
2022-04-25 12:06:38 -0400

    first commit

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,7 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.connect("mongodb+srv://janvidankhara:janvi%4025@cluster0.glrsu.mongodb.net/crpytoAPI?retryWrites=true&w=majority", connectionOptions);
 mongoose.Promise = global.Promise;
 
 module.exports = {
